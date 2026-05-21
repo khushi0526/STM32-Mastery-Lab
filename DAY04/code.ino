f911139f75ac8588dc0aa30f9a660a69aafaf366
KHUSHI B S <bskhushi2@gmail.com>
2026-05-21 23:26:18 +0530

    Create code.ino

---
+++ DAY04/code.ino
@@ -0,0 +1,16 @@
+#define ledPin D9
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  for (int i = 0; i <= 255; i++) {
+    analogWrite(ledPin, i);
+    delay(10);
+  }
+  for (int i = 255; i >= 0; i--) {
+    analogWrite(ledPin, i);
+    delay(10);
+  }
+}
